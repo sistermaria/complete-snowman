a60261f150c18f39c39567c1176d223664c84f96
sistermaria <39138688+sistermaria@users.noreply.github.com>
2021-06-26 20:21:22 -0500

    snowman
     This is a graphic of snowman using p5.js and html

---
+++ completesnowman.js
@@ -0,0 +1,28 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background('aqua');
+  ellipse(200,300,150,150);
+  ellipse(203,199,100,100);
+  ellipse(203,130,75,75);
+  ellipse( 195,120,10,10);//left eye
+  ellipse(215,120,10,10);//right eye
+  ellipse(205,140,10,10);//nose
+  ellipse(205,155,20,10);//mouth
+  line(159,200,90,90);//L arm
+  line(300,100,240,220);// R arm
+  
+  stroke(0,0,0);
+  ellipse(200,200,10,10);//button
+
+  
+  
+  
+  
+  
+  
+  
+  
+}
